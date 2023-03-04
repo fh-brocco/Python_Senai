8d31d5e9b36fefab9bc71df50f423792254100dc
Fernando Brocco <101286894+fh-brocco@users.noreply.github.com>
2023-03-04 17:44:40 -0300

    Delete Manipulação de arquivos directory

--- Manipulação de arquivos/removeArquivo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-diretorio = 'C:/Users/Aluno/Documents/Brocco/Destino' #caminho pasta de destino
-extensao = '.jpg' #extensões de arquivos de preferência
-
-for nomearquivo in os.listdir(diretorio): 
-    if nomearquivo.endswith(extensao):
-            os.remove(os.path.join(diretorio, nomearquivo)) #remoção de arquivos
